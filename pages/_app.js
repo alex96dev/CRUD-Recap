6579f2e6e8333bb929160d799f62ebe41332b048
Alexander Stracke <Alexander.Stracke@gmx.net>
2024-02-28 12:27:48 +0100

    update: _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import GlobalStyle from "../styles";
+import { uid } from "uid";
 
 export default function App({ Component, pageProps }) {
-  const [places, setPlaces] = useState();
+  const [places, setPlaces] = useState([]);
 
   function handleAddPlace(data) {
-    setPlaces([...places, data]);
+    const newPlace = { id: uid(), ...data };
+    setPlaces([...places, newPlace]);
   }
 
   console.log(places);
